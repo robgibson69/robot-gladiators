ea2e934ec0feebb86881ce52dba4891c022e9f4e
GIBBY <git_gibby@bulldogsolutions.ca>
2021-09-06 15:03:58 -0400

    add enemy combatants

--- assests/js/game.js
+++ assests/js/game.js
@@ -6,14 +6,18 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+var fight = function(enemyName) {
    // Alert players that they are starting the round
    window.alert("Welcome to Robot Gladiators!");
    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+   console.log(
+     playerName + " chose to " + promptFight
+   );
 
    // if player choses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {   
@@ -54,11 +58,13 @@ var fight = function() {
           playerMoney = playerMoney - 2;
       }
 
-  // if no (false), aks question again by running fight() again
+  // if no (false), ask question again by running fight() again
   } else {
     fight();
   }
  };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
 
