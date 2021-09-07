701ac8082ddeddc2e9f12c95fd45ddf070a9225a
Rob Gibson <rgibson@bulldogsolutions.ca>
2021-09-07 19:00:40 -0400

    Fixed - player can enter a blank name

--- assests/js/game.js
+++ assests/js/game.js
@@ -84,8 +84,9 @@ var startGame = function() {
     if (playerInfo.health > 0) {
       //let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to the Robot Gladiators! Round ' + (i + 1));
+      debugger;
 
-      // pick enw enemy ot fight based on the index of the enemy.names array
+      // pick enemy to fight based on the index of the enemy.names array
       var pickedEnemyObj = enemyInfo[i];
 
       // reset enemy.health before starting new fight
@@ -192,8 +193,21 @@ switch (shopOptionsPrompt) {
 /******** START GAME INFORMATION VARIABLES  ********/
 
 // player information
+
+//function to set name
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name")
+  }
+
+  console.log("Your robot's name is " + name);
+  return name;
+}
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
